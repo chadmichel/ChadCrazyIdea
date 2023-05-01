637f2b712184387bd52ab5802ed69f9e3490a170
= <=>
2023-05-01 08:25:05 -0500

    primeng

--- Portal/src/app/app.component.ts
+++ Portal/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Portal';
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Admin',
+        icon: 'pi-sliders-h',
+      },
+    ];
+  }
 }
